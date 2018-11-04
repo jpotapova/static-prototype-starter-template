f67be4124a97cbde65d2ec59590b2bd50a5bcbf2
Jelizaveta Potapova <potapova.jelizaveta@gmail.com>
2018-11-04 23:51:23 +0200

    cleanup js and css errors and warnings

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,10 @@
 (function(){
   "use strict";
 
-  var Author = function(name){
-    this.name = name || "Anonymous";
-    this.articles = [];
+  var hello = function(name){
+    return "Hello " + name;
   };
 
-  Author.prototype.writeArticle = function(title){
-    this.articles.push(title);
-  };
+  exports.hello = hello;
 
-  Author.prototype.listArticles = function(){
-    return this.name + " has written: " + this.articles.join(", ");
-  };
-
-  exports.Author = Author;
-
-  var peter = new Author("Peter");
-  peter.writeArticle("A Beginners Guide to npm");
-  peter.writeArticle("Using npm as a build tool");
-  peter.listArticles();
 })();
